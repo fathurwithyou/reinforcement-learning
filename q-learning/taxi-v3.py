ee1120a5f480286f5c74784a0fe0297148c5e281
Fathur <rizkyfathur326@gmail.com>
2025-07-21 11:06:15 +0700

    add comments to clarify hyperparameters and training settings in taxi-v3.py

--- q-learning/taxi-v3.py
+++ q-learning/taxi-v3.py
@@ -6,15 +6,15 @@ np.bool8 = np.bool_
 
 env = gym.make("Taxi-v3", render_mode="ansi")
 
-alpha = 0.9
-gamma = 0.95
-epsilon = 1
-epsilon_decay = 0.9995
-min_epsilon = 0.1
+alpha = 0.9 # Learning rate -> 90% of the error is used to update the Q-value
+gamma = 0.95 # Discount factor -> 95% of the future reward is considered
+epsilon = 1 # Initial exploration rate -> 100% exploration
+epsilon_decay = 0.9995 # Decay rate for exploration -> 0.05% decay per epoch
+min_epsilon = 0.1 # Minimum exploration rate -> 10% exploration
 # [(25*5*4), 6] = 500 states, 6 actions
 q_table = np.zeros((env.observation_space.n, env.action_space.n))
-epochs = 1000
-max_steps = 100
+epochs = 1000 # Number of epochs to train the agent
+max_steps = 100 # Maximum steps per epoch
 
 def choose_action(state):
     if random.uniform(0, 1) < epsilon:
